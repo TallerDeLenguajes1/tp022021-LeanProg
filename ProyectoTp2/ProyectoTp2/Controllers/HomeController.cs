5730fcdd2c31646d8380514543413e4fee321a78
LeanProg <leomasterx10@gmail.com>
2021-09-22 19:08:42 -0300

    TP2 terminado Ortega Leandro

---
+++ ProyectoTp2/ProyectoTp2/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProyectoTp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net;
+
+namespace ProyectoTp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Empleado()
+        {
+
+            return View();
+        }
+        public IActionResult VerEmpleado(string nombre, string apellido,string dire, string Fna, string Fing, string titulo, long matri)
+        {
+            try
+            {
+                DateTime fechaNa =  DateTime.Parse(Fna);
+                DateTime fechaing =  DateTime.Parse(Fing);
+                if (nombre!=null && apellido!=null && dire!=null &&titulo!=null)
+                {
+                    Empleado empleado = new Empleado(nombre,apellido,dire,fechaNa,fechaing,titulo,matri);
+                    int antiguedad = empleado.DevolverAntiguedad();
+                    int edad = empleado.EdadEmpleado();
+                    double salario = empleado.SalarioEmpleado();
+                    logger.Info("Datos del empleado: "+ " name: " + nombre + " Apelldio: " + apellido + " Edad: " + edad +" Salario: " + salario + " Antiguedad" + antiguedad );
+                    
+                }
+                else
+                {
+                    throw new ArgumentNullException("Parameters can't be null.");
+                }
+               
+                
+            }
+            catch (Exception ex)
+            {
+                
+                logger.Error(ex.Message);
+               
+            }
+
+            return View();
+        }
+       
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProyectoTp2/ProyectoTp2/Empleado.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoTp2
+{
+    public class Empleado
+    {
+
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private DateTime fechaNacimiento;
+        private DateTime fechaIngreso;
+        private string titulo;
+        private long matricula;
+        private static double sueldobasico = 70000;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public long Matricula { get => matricula; set => matricula = value; }
+        public static double Sueldobasico { get => sueldobasico; set => sueldobasico = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+        public Empleado(string nombre, string apellido, string direccion,DateTime fechaNacimiento, DateTime fechaIngreso, string titulo, long matricula)
+        {
+            this.Nombre = nombre;
+            this.apellido = apellido;
+            this.Direccion = direccion;
+            this.fechaNacimiento = fechaNacimiento;
+            this.FechaIngreso = fechaIngreso;
+            this.Titulo = titulo;
+            this.Matricula = matricula;
+            
+        }
+
+        //Calcular antiguedad
+        public int DevolverAntiguedad()
+        {
+            int fechaActual = Convert.ToInt32(DateTime.Now.Year);
+            int count=0;
+            int fechaempleado = Convert.ToInt32(this.fechaIngreso.Year);
+            while (fechaempleado<fechaActual)
+            {
+                count++;
+                fechaempleado++;
+            }
+            return count;
+        }
+        //FUNCION PARA CALCULAR EDAD DEL EMPLEADO
+        public int EdadEmpleado()
+        {
+            int fechaActual = Convert.ToInt32(DateTime.Now.Year);
+            int edad; ;
+            edad = fechaActual - Convert.ToInt32(this.fechaNacimiento.Year);
+            return edad;
+        }
+        //FUNCION PARA CALCULAR SALARIO DEL EMPLEADO
+        public double SalarioEmpleado()
+        {
+            int antiguedad = this.DevolverAntiguedad();
+            double salario = 0;
+
+            if (antiguedad >= 1 && antiguedad < 20)
+            {
+                salario = sueldobasico + ((1 / 100) * sueldobasico * antiguedad) - 0.15 * sueldobasico;
+                return salario;
+            }
+            else
+            {
+                salario = sueldobasico + ((1 / 100) * sueldobasico * 25) - 0.15 * sueldobasico;
+                return salario;
+            }
+        
+            
+        }
+
+
+        
+
+
+    }
+
+}
+
+   
